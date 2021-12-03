bbfd24187409f9d360f461c1f8f2c327672a8329
Timemanchik <timemanchik@mail.ru>
2021-12-03 23:57:32 +0300

    3 commit

--- society_name.rb
+++ society_name.rb
@@ -14,3 +14,18 @@ for el in 0.. (y - 1)
     x[el] = gets
 end
 society_name(x)
+
+/
+A group of friends have decided to start a secret society. The name will be the first letter of each of their names, sorted in alphabetical order.
+
+Create a function that takes in an array of names and returns the name of the secret society.
+
+Examples
+society_name(["Adam", "Sarah", "Malcolm"]) ➞ "AMS"
+
+society_name(["Harry", "Newt", "Luna", "Cho"]) ➞ "CHLN"
+
+society_name(["Phoebe", "Chandler", "Rachel", "Ross", "Monica", "Joey"]) ➞ "CJMPRR"
+
+Notes
+The secret society's name should be entirely uppercased.
