6342a88777c6009835639c671b776c1112e69652
Timemanchik <timemanchik@mail.ru>
2021-12-03 18:02:28 +0300

    3 commit

--- calculate_exponent.rb
+++ calculate_exponent.rb
@@ -1,6 +1,7 @@
 def calculate_exponent(num, exp)
-	num <= 0 || exp <= 0 || num % 1 != 0 || exp % 1 != 0 ? puts("oshibka. tolko polozhitelnye celye chisla") : num ** exp
+	num % 1 != 0 || exp % 1 != 0 || num <= 0 || exp <= 0 ? puts("oshibka. tolko polozhitelnye celye chisla") : num.to_i ** exp.to_i
 end
 x = gets.to_f
 y = gets.to_f
-calculate_exponent(x.to_i, y.to_i)
+
+calculate_exponent(x, y)
